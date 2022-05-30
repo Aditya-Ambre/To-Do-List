e670b01dcb50d361290efa1e9468cf110711fa85
Aditya Ambre <adityaambre17feb@gmail.com>
2022-05-30 15:33:17 +0530

    fixed bug

--- script.js
+++ script.js
@@ -1,59 +1,86 @@
+console.log("Welcome to notes app. This is app.js");
+showNotes();
 
-function getAndUpdate() {
-  console.log("Updating List...");
-  tit = document.getElementById("title").value;
-  desc = document.getElementById("description").value;
-  if (localStorage.getItem("itemsJson") == null) {
-    itemJsonArray = [];
-    itemJsonArray.push([tit, desc]);
-    localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+// If user adds a note, add it to the localStorage
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
   } else {
-    itemJsonArrayStr = localStorage.getItem("itemsJson");
-    itemJsonArray = JSON.parse(itemJsonArrayStr);
-    itemJsonArray.push([tit, desc]);
-    localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+    notesObj = JSON.parse(notes);
   }
-  update();
-}
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  //   console.log(notesObj);
+  showNotes();
+});
 
-function update() {
-  if (localStorage.getItem("itemsJson") == null) {
-    itemJsonArray = [];
-    localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+// Function to show elements from localStorage
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
   } else {
-    itemJsonArrayStr = localStorage.getItem("itemsJson");
-    itemJsonArray = JSON.parse(itemJsonArrayStr);
+    notesObj = JSON.parse(notes);
   }
-  // Populate the table
-  let tableBody = document.getElementById("tableBody");
-  let str = "";
-  itemJsonArray.forEach((element, index) => {
-    str += `
-                    <tr>
-                    <th scope="row">${index + 1}</th>
-                    <td>${element[0]}</td>
-                    <td>${element[1]}</td> 
-                    <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td> 
-                    </tr>`;
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Note ${index + 1}</h5>
+                        <p class="card-text"> ${element}</p>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                    </div>
+                </div>`;
   });
-  tableBody.innerHTML = str;
-}
-add = document.getElementById("add");
-add.addEventListener("click", getAndUpdate);
-update();
-function deleted(itemIndex) {
-  console.log("Delete", itemIndex);
-  itemJsonArrayStr = localStorage.getItem("itemsJson");
-  itemJsonArray = JSON.parse(itemJsonArrayStr);
-  // Delete itemIndex element from the array
-  itemJsonArray.splice(itemIndex, 1);
-  localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
-  update();
-}
-function clearStorage() {
-  if (confirm("Do you areally want to clear?")) {
-    console.log("Clearing the storage");
-    localStorage.clear();
-    update();
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
   }
 }
+
+// Function to delete a note
+function deleteNote(index) {
+  //   console.log("I am deleting", index);
+
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
+
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function () {
+  let inputVal = search.value.toLowerCase();
+  // console.log('Input event fired!', inputVal);
+  let noteCards = document.getElementsByClassName("noteCard");
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+    // console.log(cardTxt);
+  });
+});
+
+/*
+Further Features:
+1. Add Title
+2. Mark a note as Important
+3. Separate notes by user
+4. Sync and host to web server 
+*/
